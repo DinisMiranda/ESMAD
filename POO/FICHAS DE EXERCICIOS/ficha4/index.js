f121e39a81e8b8ab184a09ae72351046b2f6e5fb
DinisMiranda <40240120@esmad.ipp.pt>
2025-03-24 10:38:40 +0000

    Update index.js

--- POO/FICHAS DE EXERCICIOS/ficha4/index.js
+++ POO/FICHAS DE EXERCICIOS/ficha4/index.js
@@ -5,8 +5,35 @@ import * as data from "./data.js";
 
 let listUsers = []
 listUsers=  data.init();
-
-//console.log(listUsers)
-
+console.log(listUsers)
 
 
+function listarNivel(nivel) {
+    let tabelaNivel = listUsers.filter(element => element.nivel == nivel);
+    let texto="users no nivel ${nivel}:"
+    for (letjogador of tabelaNivel) {
+        texto += `
+        ${jogador.nome}`
+    }
+    alert(texto);
+}
+
+
+function somarPontos(nivel) {
+    let tabelaNivel = listUsers.filter(element => element.nivel == nivel);
+    let soma=0;
+    for (let jogador of tabelaNivel) {
+        soma += jogador.pontos;
+    }
+    alert(`Soma dos pontos no nivel ${nivel}: ${soma}`);
+}
+
+function VerificarNivel(nivel) {
+    let tabelaNivel = listUsers.filter(element => element.nivel == nivel);
+    let texto = "users no nivel ${nivel}:"
+    for (let jogador of tabelaNivel) {
+        texto += `
+        ${jogador.nome}`
+    }
+    alert(texto);
+}
